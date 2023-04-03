bde1ecd485d715b7da9b4763dfb94519599ebf5e
Phyo Kyi <phyokyi1986@gmail.com>
2023-04-03 22:33:40 +0630

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ def main():
     mmr_input_text = st.text_input("နာမည်လေးရိုက်ထည့်ပေးပါ... (မြန်မာလိုပဲ ရိုက်ထည့်ပေးပါ)")
     mmr_output_text = ""
     if st.button("အဖြေရဖို့နှိပ်ပါ"):
-        result = manual_test(clean_text(mmr_input_text))
-        if result==1:
-            mmr_output_text = mmr_output_text + "\nလက်ရှိသုံးနေတဲ့ Data များအရ " + mmr_input_text + " ဆိုတဲ့ နာမည်က မိန်းကလေး အများစု သုံးပါတယ်။"
-        elif result==0:
-            mmr_output_text = mmr_output_text + "\nလက်ရှိသုံးနေတဲ့ Data များအရ " + mmr_input_text + " ဆိုတဲ့ နာမည်က ယောက်ျားလေး အများစု သုံးပါတယ်။"
-        else:
-            mmr_output_text = mmr_output_text + "\nလက်ရှိသုံးနေတဲ့ Data များအရ " + mmr_input_text + " ဆိုတဲ့ နာမည်က ယောက်ျားလေး ရော မိန်းကလေး ပါ ဖြစ်နိုင်ပါတယ်။"
-        st.write(mmr_output_text)
+        if len(mmr_input_text.strip())>0:
+            result = manual_test(clean_text(mmr_input_text))
+            if result==1:
+                mmr_output_text = mmr_output_text + "\nလက်ရှိသုံးနေတဲ့ Data များအရ " + mmr_input_text + " ဆိုတဲ့ နာမည်က မိန်းကလေး အများစု သုံးပါတယ်။"
+            elif result==0:
+                mmr_output_text = mmr_output_text + "\nလက်ရှိသုံးနေတဲ့ Data များအရ " + mmr_input_text + " ဆိုတဲ့ နာမည်က ယောက်ျားလေး အများစု သုံးပါတယ်။"
+            else:
+                mmr_output_text = mmr_output_text + "\nလက်ရှိသုံးနေတဲ့ Data များအရ " + mmr_input_text + " ဆိုတဲ့ နာမည်က ယောက်ျားလေး ရော မိန်းကလေး ပါ ဖြစ်နိုင်ပါတယ်။"
+            st.write(mmr_output_text)
     
     st.title("Let's try to classify Burmese Names by sex")
     
@@ -23,15 +24,17 @@ def main():
     eng_output_text = ""
     
     if st.button("Click here to get answer"):
-        translate_test = convert_en_to_mm(eng_input_text)
-        result = manual_test(clean_text(translate_test))
-        if result==1:
-            eng_output_text = eng_output_text + "\nAccording current dataset, " + eng_input_text + " is use by mostly female"
-        elif result==0:
-            eng_output_text = eng_output_text + "\nAccording current dataset, " + eng_input_text + " is use by mostly male"
-        else:
-            eng_output_text = eng_output_text + "\nAccording current dataset, " + eng_input_text + " can be both male and female."
-        st.write(eng_output_text)
+        if len(eng_input_text.strip())>0:
+            translate_test = convert_en_to_mm(eng_input_text)
+            result = manual_test(clean_text(translate_test))
+            if result==1:
+                #According to the current dataset, .... is mostly used by male.
+                eng_output_text = eng_output_text + "\nAccording to the current dataset, " + eng_input_text + " is mostly used by female."
+            elif result==0:
+                eng_output_text = eng_output_text + "\nAccording to the current dataset, " + eng_input_text + " is mostly used by male."
+            else:
+                eng_output_text = eng_output_text + "\nAccording to the current dataset, " + eng_input_text + " can be both male and female."
+            st.write(eng_output_text)
 
     st.title("Report Name")
     st.text("If you found wrong one or new names, please submit the right one via following link...")
